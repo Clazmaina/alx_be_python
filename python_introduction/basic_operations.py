ed6c65387ba1e0a90f44c47aff21eb2730651bdd
Claire Wanjiru <claywangari@gmail.com>
2024-12-13 18:06:43 +0200

    First commit of python arithmetic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
 number1 = 10
 number2 = 5
 sum_of_variables = number1 + number2
-print("Addition of", number1, "and", number2, "is:", sum_of_variables)
+print("Addition of", number1, "and", number2, "is", sum_of_variables)
 difference = number1 - number2
-print("Subtraction of", number1, "and", number2, "is:", difference)
+print("Subtraction of", number1, "and", number2, "is", difference)
 product = number1 * number2
-print("Multiplication of", number1, "and", number2, "is:", product)
+print("Multiplication of", number1, "and", number2, "is", product)
