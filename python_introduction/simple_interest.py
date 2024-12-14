5ad5fe26f2409911fb7d5b96e64c04d678d20127
Claywangari <claywangari@gmail.com>
2024-12-14 12:20:41 +0200

    A calculation of simple interest on python

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+principal = 1000
+rate = 0.05
+time = 3
+print("principal:", principal)
+print("rate:", rate)
+print("time:", time)
+I = "interest earned"
+print(I)
+P =int(input("Enter principal amount:"))
+R =float(input("Enter Rate of Interest:"))
+T =float(input("Enter Time in Year:"))
+I=(P*R*T)/100
+print("The simple interest is:",I)
+
