394e108ff900bf7015ddf08bc68efc808edfdcf2
Clazmaina <claywangari@gmail.com>
2024-12-22 16:45:27 +0200

    Match Case statements for handling multiple operations in a simple calculator program.

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -15,6 +15,8 @@ def match_case_calculator(num1,operation,num2):
         case'_':
             print("Invalid operation. Please use +,-,*, or /.")
             return None
-
+first_number = input("Enter the first number:")
+second_number = input("Enter the second number:")
+operation = input("Choose the operation (+,-,*,/):")
 result = match_case_calculator(28,"*",5)
 print(f"The result is: {result}")
