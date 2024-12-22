8b0ff0c6fa97728ef80d0edd2e34f02bb5041be5
Clazmaina <claywangari@gmail.com>
2024-12-22 21:13:29 +0200

    This script utilizes conditional statements to guide program execution based on user input regarding weather conditions.

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,14 +1,14 @@
 def get_clothing_recommendation():
-    weather_conditions = ["sunny", "rainy", "cold"]
+ weather_conditions = ["sunny", "rainy", "cold"]
 user = input("What's the weather like today?(sunny/rainy/cold):")
 
 if user =="sunny":
-        print("Wear a t-shirt and sunglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif user=="rainy":
-        print("Don't forget your umbrella and a raincoat.")
+    print("Don't forget your umbrella and a raincoat.")
 elif user=="cold":
-        print("Make sure to wear a warm coat and a scarf.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-        print("Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
 if __name__=="__main__":
-    weather = get_clothing_recommendation()
+  weather = get_clothing_recommendation()
