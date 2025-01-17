42c727c5d1d030200225c7caf5a52095cdfbd0c4
Clazmaina <claywangari@gmail.com>
2025-01-17 15:09:25 +0200

    Writing Unit Tests for a Simple Calculator Class.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,18 +13,20 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.add(5,-2),3)
         self.assertEqual(self.calculator.add(0,5),5)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract)
         self.assertEqual(self.calculator.subtract(5, 3), 2)
         self.assertEqual(self.calculator.subtract(1, -1), 2)
         self.assertEqual(self.calculator.subtract(0, 0), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply)
         self.assertEqual(self.calculator.multiply(2, 3), 6)
         self.assertEqual(self.calculator.multiply(-2, 3), -6)
         self.assertEqual(self.calculator.multiply(0, 5), 0)
 
-    def test_divide(self):
+    def test_division(self):
+        self.assertEqual(self.calc.divide)
         self.assertEqual(self.calculator.divide(10, 2), 5)
         self.assertEqual(self.calculator.divide(-10, 2), -5)
         self.assertEqual(self.calculator.divide(10, 0), None)  # Test for division by zero
